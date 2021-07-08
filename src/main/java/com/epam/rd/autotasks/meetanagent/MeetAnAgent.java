95642d7ed2456be58c6aca2c93d997e82f3aec0c
Dmitry <guapower@yandex.ru>
2021-07-08 17:38:53 +0000

    add comments

--- src/main/java/com/epam/rd/autotasks/meetanagent/MeetAnAgent.java
+++ src/main/java/com/epam/rd/autotasks/meetanagent/MeetAnAgent.java
@@ -4,6 +4,9 @@ public class MeetAnAgent {
     final static String password = "badpassword"; //You can change pass, but don't change the type
 
     public static void main(String[] args) {
-        //Write a code
+        //write a program that asks for an input string.
+        //If input line equals to secret password, program prints "Hello, Agent".
+        //Otherwise prints "Access denied".
+        
     }
 }
