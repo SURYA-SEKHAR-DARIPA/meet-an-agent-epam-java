fe07373979931a837b1aeb29d9fb258c2ab15541
Evgenii_Efimchik <evgenii_efimchik@epam.com>
2021-10-12 15:19:31 +0300

    change Password
    (cherry picked from commit 51342a0fe6dba5bde41314d3a57203b94915f4bd)

--- src/main/java/com/epam/rd/autotasks/meetanagent/MeetAnAgent.java
+++ src/main/java/com/epam/rd/autotasks/meetanagent/MeetAnAgent.java
@@ -1,7 +1,7 @@
 package com.epam.rd.autotasks.meetanagent;
 
 public class MeetAnAgent {
-    final static String password = "badpassword"; //You can change pass, but don't change the type
+    final static String password = "Swordfish"; //You can change pass, but don't change the type
 
     public static void main(String[] args) {
         //write a program that asks for an input string.
